9115e2904130b59b30cbff553c5f9efed07b3d98
Juan Jose Francisco R <jj.francisco.ramirez@gmail.com>
2022-07-13 22:31:28 -0400

    cleaning ui api client

---
+++ src/FinChatter.WebUI.Core/Service/FinChatterApiService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FinChatter.API.Client;
+using FinChatter.API.Contracts.Request;
+using FinChatter.API.Contracts.Response;
+
+namespace FinChatter.WebUI.Core.Service
+{
+    public class FinChatterApiService
+    {
+        private readonly IFinChatterApiClient _apiClient;
+
+        public FinChatterApiService(IFinChatterApiClient apiClient)
+        {
+            _apiClient = apiClient;
+        }
+
+        public async Task<ClientResponse<RegisterResponse>> Register(RegisterRequest request) => await _apiClient.Register(request);
+        public async Task<ClientResponse<LoginResponse>> Login(LoginRequest request) => await _apiClient.Login(request);
+    }
+}
--- src/FinChatter.WebUI/Program.cs
+++ src/FinChatter.WebUI/Program.cs
@@ -1,8 +1,12 @@
+using FinChatter.API.Client;
 using FinChatter.WebUI.Core;
 using FinChatter.WebUI.Core.AuthProviders;
+using FinChatter.WebUI.Core.Service;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Refit;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,5 +15,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<AuthenticationStateProvider, TestAuthStateProvider>();
+builder.Services.AddSingleton<IFinChatterApiClient>(RestService.For<IFinChatterApiClient>("https://localhost:7208"));
+builder.Services.AddSingleton<FinChatterApiService>();
 
 await builder.Build().RunAsync();
