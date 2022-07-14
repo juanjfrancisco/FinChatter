ae8942e81125390d5dadc111ed78dbc4c7bd83d2
Juan Jose Francisco R <jj.francisco.ramirez@gmail.com>
2022-07-14 12:14:26 -0400

    Integrate chatClient service with chat ui

--- src/FinChatter.WebUI.Core/Service/ChatClient.cs
+++ src/FinChatter.WebUI.Core/Service/ChatClient.cs
@@ -17,31 +17,22 @@ namespace FinChatter.WebUI.Core.Service
 
         public async Task StartConnection(string username, Action<ChatMessage> handler)
         {
-            Console.WriteLine(username);
             var hubUrl = await _localStorage.GetItemAsync<string>("finChatterUrl");
-            var token = await _localStorage.GetItemAsync<string>("authToken");
-            Console.WriteLine($"Get url: {hubUrl} token: {token}");
+
             ChatHubConnection = new HubConnectionBuilder()
                 .WithUrl(hubUrl, options =>
                 {
                     options.AccessTokenProvider = async () =>
                     {
-                        return token;
+                        return await _localStorage.GetItemAsync<string>("authToken");
                     };
                 })
                 .Build();
 
-            Console.WriteLine("after build");
-
             ChatHubConnection.On<ChatMessage>("SendMessage", handler);
 
-            Console.WriteLine("after connection");
             await ChatHubConnection.StartAsync();
-            Console.WriteLine("after start");
-
             await SendAsync($"[Notice] {username} joined chat room.", username);
-
-            Console.WriteLine("after notice");
         }
 
         public async Task SendAsync(string message, string username)
